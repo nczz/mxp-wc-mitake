04adec006e65a0f30d6ba93867472a62b2270506
Chiang Hung Chun <im@mxp.tw>
2019-08-23 02:48:05 +0800

    init

---
+++ main.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('WPINC')) {
+	die;
+}
+
+if (!empty($_POST) && wp_verify_nonce($_REQUEST['_wpnonce'], 'mxp-wc-mitake-main-setting-page')) {
+	$account = $_POST['mxp_mitake_account'];
+	$password = $_POST['mxp_mitake_password'];
+	$msg_body = $_POST['mxp_mitake_msg_body'];
+	$debug_mode = $_POST['mxp_mitake_debug_mode'];
+	if (isset($password) && isset($account) && !empty($password) && !empty($account) && !empty($msg_body) && !empty($debug_mode)) {
+		update_option("mxp_mitake_account", $account);
+		update_option("mxp_mitake_password", $password);
+		update_option("mxp_mitake_msg_body", $msg_body);
+		update_option("mxp_mitake_debug_mode", $debug_mode);
+		echo "更新完成</br>";
+	}
+}
+
+?>
+<form action="" method="POST">
+	三竹帳號：<input type="text" value="<?php echo get_option("mxp_mitake_account"); ?>" name="mxp_mitake_account" size="20"  /></br>
+	三竹密碼：<input type="text" value="<?php echo get_option("mxp_mitake_password"); ?>" name="mxp_mitake_password" size="20"  /></br>
+	簡訊文字：<textarea name="mxp_mitake_msg_body" rows="3" cols="40"><?php echo get_option("mxp_mitake_msg_body", ""); ?></textarea></br>
+	剩餘點數：<?php echo (get_option("mxp_mitake_account", "") != "" && get_option("mxp_mitake_password", "") != "") ? mxp_mitake_get_points(get_option("mxp_mitake_account"), get_option("mxp_mitake_password"), get_option("mxp_mitake_debug_mode", "no")) : "請先輸入帳密後進行查詢"; ?></br>
+	除錯模式：<select name="mxp_mitake_debug_mode"><option value="no" <?php selected(get_option("mxp_mitake_debug_mode"), "no");?>>否</option><option value="yes" <?php selected(get_option("mxp_mitake_debug_mode"), "yes");?>>是</option></select></br>
+ <input type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce('mxp-wc-mitake-main-setting-page'); ?>"/>
+<p><input type="submit" id="save" value="儲存設定" class="button action" /></p>
+</form>
+<p>外掛版本：v1.0.0</p>
+<p>作者：<a href="https://www.mxp.tw/contact/" target="blank">江弘竣（阿竣）Chun</a></p>
+<p>請作者咖啡：<a href="https://mxp.tw/lw" target="blank">鼓勵一下</a></p>
---
+++ mxp-wc-mitake.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Plugin Name: WooCommerce 三竹簡訊整合
+Plugin URI: https://github.com/nczz/mxp-wc-mitake
+Description: 設定傳送完成訂單後的簡訊
+Author: Chun
+Version: 1.0
+Author URI: https://www.mxp.tw/
+ */
+
+/**
+ ** @param username 三竹簡訊用戶帳號
+ ** @param password 三竹簡訊用戶密碼
+ ** @param mobile 台灣手機號碼
+ ** @param text 簡訊內容。切勿混用非中英文等其他語言。
+ **/
+
+function mxp_mitake_send_sms($username, $password, $mobile, $text, $debug = "no") {
+	$package = array(
+		'username' => $username,
+		'password' => $password,
+		'dstaddr' => $mobile,
+		'smbody' => $text,
+		'encoding' => 'UTF8',
+	);
+
+	$url = 'http://smsapi.mitake.com.tw/api/mtk/SmSend?' . http_build_query($package, '', '&', PHP_QUERY_RFC3986);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_VERBOSE, true);
+	if ($debug == "yes") {
+		$verbose = fopen('php://temp', 'w+');
+		curl_setopt($ch, CURLOPT_STDERR, $verbose);
+	}
+	$output = curl_exec($ch);
+	$verboseLog = "";
+	if ($debug) {
+		rewind($verbose);
+		$verboseLog = stream_get_contents($verbose);
+	}
+	curl_close($ch);
+	return $verboseLog . $output;
+}
+
+function mxp_mitake_get_points($username, $password, $debug = "no") {
+	$package = array(
+		'username' => $username,
+		'password' => $password,
+	);
+
+	$url = "http://smsapi.mitake.com.tw/api/mtk/SmQuery?" . http_build_query($package, '', '&', PHP_QUERY_RFC3986);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_VERBOSE, true);
+	if ($debug) {
+		$verbose = fopen('php://temp', 'w+');
+		curl_setopt($ch, CURLOPT_STDERR, $verbose);
+	}
+	$output = curl_exec($ch);
+	$verboseLog = "";
+	if ($debug == "yes") {
+		rewind($verbose);
+		$verboseLog = stream_get_contents($verbose);
+	}
+	curl_close($ch);
+	return $verboseLog . $output;
+}
+
+function register_mxp_wc_mitake_custom_submenu_page() {
+	add_submenu_page('woocommerce', '三竹簡訊整合', '三竹簡訊整合', 'manage_options', 'my-custom-submenu-page', 'mxp_wc_mitake_submenu_page_callback');
+}
+
+function mxp_wc_mitake_submenu_page_callback() {
+	include plugin_dir_path(__FILE__) . "/main.php";
+}
+add_action('admin_menu', 'register_mxp_wc_mitake_custom_submenu_page', 99);
+
+function mxp_sms_payment_complete_hook($order_id) {
+	$order = wc_get_order($order_id);
+	$user = $order->get_user();
+	// $order->get_billing_email()
+	$mobile = $order->get_billing_phone();
+	// $order->get_billing_first_name()
+	// $order->get_billing_last_name()
+	$username = get_option("mxp_mitake_account", "");
+	$password = get_option("mxp_mitake_password", "");
+	$msg = get_option("mxp_mitake_msg_body", "");
+	if (strpos($mobile, '09') === 0 && strlen($mobile) == 10) {
+		if ($username != "" && $password != "" && $msg != "") {
+			$resp = mxp_mitake_send_sms($username, $password, $mobile, $msg, get_option("mxp_mitake_debug_mode", "no"));
+			$order->add_order_note("已傳送簡訊「" . $msg . "」至「" . $order->get_billing_last_name() . $order->get_billing_first_name() . "」手機「" . $order->get_billing_phone() . "」。" . PHP_EOL . "三竹簡訊 API 回應：" . PHP_EOL . $resp);
+		} else {
+			$order->add_order_note("簡訊設定參數錯誤，無法發送簡訊。");
+		}
+	} else {
+		$order->add_order_note("手機格式錯誤「{$mobile}」，無法發送簡訊。");
+	}
+}
+add_action('woocommerce_order_status_completed', 'mxp_sms_payment_complete_hook');
